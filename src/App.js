29e833f8c6efc89289c5ce3af95ff3e8d3dae096
Daniel Soto <dasg.1993@gmail.com>
2021-10-16 09:57:26 -0500

    [feature] - edit & remove employee

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import { AddEmployee } from './components/add-employee';
 import { GlobalProvider } from './context/GlobalState';
 import { Home } from './components/home';
+import { EditEmployee } from './components/edit-employee';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/add" component={AddEmployee} />
+          <Route path="/edit/:id" component={EditEmployee} />
         </Switch>
       </div>
     </GlobalProvider>
